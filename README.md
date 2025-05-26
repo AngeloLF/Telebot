6ad4576602c06a09507b24498fe9b6a38c6bd690
AngeloLF <angelo.lamure@gmail.com>
2025-05-26 19:59:14 +0200

    big upd

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Telebot
 Telegram bot.
+
+## Pip install
+
+* `pip install python-telegram-bot`
+* `pip install coloralf`
--- telebot.py
+++ telebot.py
@@ -1,56 +1,82 @@
 from telegram import Bot, InputFile
-import asyncio
 import coloralf as c
-import sys, os
+import os, sys, asyncio
 path_home = os.path.expanduser("~/")
+import params
 
 
 
 
-async def main(text=None, image=None):
+async def asyncSending(texts, bot_token, userIDs, printage=True):
+    """
+    Fonction asynchrone qui permet d'envoyer le message
 
-    TOKEN_file = f"{path_home}ccalf.txt"
-    ID_file = f"{path_home}alfid.txt"
+    Params:
+        texts [lsit(str)]   : liste d'str a envoyer par le bot
+        bot_token [str]     : token du bot
+        userIDs [lsit(str)] : liste des IDs des users
+        printage [bool]     : Si True, affiche les print pour chaque envoie
+    """
 
-    with open(f"{TOKEN_file}", "r") as f : TOKEN = f.read()
-    with open(f"{ID_file}", "r") as f : CHAT_ID = f.read()
 
     try:
 
-        bot = Bot(TOKEN)
+        bot = Bot(bot_token)
         bot_name_class = await bot.getMyName()
         bot_name = bot_name_class.name
 
-        if text is not None:
 
-            texts = text if isinstance(text, (list)) else [text]
+        # On itère sur les texts
+        for t in texts:
 
-            for t in texts:
-                await bot.send_message(chat_id=CHAT_ID, text=f"{t}")
-                print(f"{c.ti}<{bot_name}> send : {t}{c.d}")
+            # On itère sur les users
+            for user in userIDs:
 
-
-        if image is not None:
-
-            images = image if isinstance(image, (list)) else [image]
-
-            for i in images:
-                with open(i, 'rb') as photo_file:
-                    await bot.send_photo(chat_id=CHAT_ID, photo=InputFile(photo_file))
-                    print(f"{c.ti}<{bot_name}> send image : {i}{c.d}")
+                await bot.send_message(chat_id=user, text=f"{t}")
+                if printage : print(f"{c.ti}<{c.y}{bot_name}{c.d}{c.ti}> send to ID={c.ly}{user}{c.d}{c.ti} : {c.y}{t}{c.d}")
 
     except Exception as e:
 
         print(f"{c.r}WARNING [in telebot.py] : {e}{c.d}")
 
 
-def send_message(text, folder_):
 
-    asyncio.run(main(text=text))
 
-def send_image(text=None, image=None):
+def send_message(texts, folder_ids=None, folder_bot=None):
+    """
+    Fonction send_message. Permet d'envoyer le(s) str donné dans `texts`
 
-    asyncio.run(main(text=text, image=image))
+    Params:
+        texts [str, list[str]] : Str ou list d'str a envoyer par le bot
+        folder_ids [str]       : Dossier avec des `.txt` qui contienne les users IDs. Si `None`, le FOLDER_IDS donné par `params.py` est choisi (prefered).
+        folder_bot [str]       : Dossier avec `bot_token.txt` qui contient le token du bot. Si `None`, le FOLDER_BOT donné par `params.py` est choisi (prefered).
+    """
+
+
+    # Define folders with users IDs and bot token
+    folder_ids = params.FOLDER_IDS if folder_ids is None else folder_ids
+    folder_bot = params.FOLDER_BOT if folder_bot is None else folder_bot
+
+
+    # Recup. user IDs
+    userIDs = list()
+    for file in os.listdir(folder_ids):
+        with open(f"{folder_ids}/{file}", "r") as f: 
+            userIDs.append(f.read())
+
+
+    # Recup. bot token
+    with open(f"{folder_bot}/bot_token.txt", "r") as f:
+        bot_token = f.read() 
+
+
+    # Si texts est un str, on le met dans une liste
+    texts = text if isinstance(text, (list)) else [text]
+
+
+    # run asyncSending 
+    asyncio.run(asyncSending(texts, bot_token, userIDs))
+
 
 
 
@@ -62,30 +88,22 @@ if __name__ == '__main__':
 
         print(f"Begin test ...")
 
-        if "message" in sys.argv[1:] or "m" in sys.argv[1:]:
-
-            send_message("Test solo")
-            send_message(["Test combiner...", 12.3, "... c'est bon"])
-
-        if "image" in sys.argv[1:] or "s" in sys.argv[1:]:
-
-            send_image(text="Voici chou : ", image=["./chou128.png"])
+        send_message("Test solo")
+        send_message(["Test combiner...", 12.3, "... c'est bon"])
 
         print(f"Test end.")
     
     else:
 
         text = None
-        TOKEN_file = None
-        ID_file = None
+        folder_ids = None
+        folder_bot = None
 
         for argv in sys.argv[1:]:
 
-            if argv[:4] == "msg=" : text = argv[4:]
-            if argv[:6] == "token=" : TOKEN_file = argv[6:]
-            if argv[:3] == "id=" : ID_file = argv[3:]
+            if argv[:5] == "text=" : text = argv[5:]
+            if argv[:5] == "fids=" : folder_ids = argv[5:]
+            if argv[:5] == "fbot=" : folder_bot = argv[5:]
 
-        with open(f"{TOKEN_file}", "r") as f : TOKEN = f.read()
-        with open(f"{ID_file}", "r") as f : CHAT_ID = f.read()
 
-        send_message(text)
+        send_message(text, folder_ids=folder_ids, folder_bot=folder_bot)
